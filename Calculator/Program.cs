db1de015b3c46c92149416cb5efede2a11d44b61
Adriano Menezes <adrianocgm@gmail.com>
2024-01-11 09:51:47 -0300

    Atualizar repositorio
    Atualizar repositorio

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tela();
+        }
+
+        private static void Tela()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
